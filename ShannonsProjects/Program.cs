e6e8f2deaea64323e91a2b0defd859ddeba493b6
Shannon Lee <shanster66@aol.com>
2013-05-13 19:55:33 -0400

    First Upload

---
+++ ShannonsProjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShannonsProjects
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mark P", "Shawn L", "Jenna L", "Benji R" };
+
+            Console.WriteLine("Enter a name to search");
+          
+            string name = Console.ReadLine();          
+ 
+
+            foreach(string item in names)
+            {
+                if (item.Contains(name))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
